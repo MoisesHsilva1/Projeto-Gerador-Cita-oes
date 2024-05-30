12d886c9898d3faf9ee9ac14be746e01fe9f60fb
Moises Henrique <moihgm67@gmail.com>
2024-05-29 21:23:06 -0300

    Atualizando Projeto

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const citacoes = [
+    "A vida é o que acontece enquanto você está ocupado fazendo outros planos. - John Lennon",
+    "O segredo da vida não é ter tudo que você quer, mas amar tudo que você tem. - George Carlin",
+    "Acredite que você pode, assim você já está no meio do caminho. - Theodore Roosevelt",
+    "O sucesso é ir de fracasso em fracasso sem perder o entusiasmo. - Winston Churchill",
+    "A única maneira de fazer um excelente trabalho é amar o que você faz. - Steve Jobs",
+    "A melhor maneira de prever o futuro é inventá-lo. - Alan Kay",
+    "A sorte favorece a mente bem preparada. - Louis Pasteur",
+    "Tudo o que você pode imaginar é real. - Pablo Picasso",
+    "A persistência é o caminho do êxito. - Charles Chaplin"
 
 ]
+var GerarRandomNumber = 
 
 function GerarCitacoes () {
-    
+
 }
 
 document.querySelector('.buttonGerar').addEventListener('click', function() {
